2e33b23fb80e9ca6726ac31cf053ed731fb833c5
piela001 <piela001@hotmail.com>
2015-03-07 18:47:44 -0600

    Integrated Damage Reports progress.  Added location permissions to manifest.

--- app/src/main/java/u/ready_wisc/DamageReports.java
+++ app/src/main/java/u/ready_wisc/DamageReports.java
@@ -1,13 +1,20 @@
 package u.ready_wisc;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.os.Bundle;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,29 +23,76 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.SimpleAdapter;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentActivity;
 
 public class DamageReports extends ActionBarActivity {
 
+    //Variable declare.
     private static EditText text9;
     Button btnTakePhoto;
     ImageView imgTakenPhoto;
+    Button btnSubmit;
     private static final int CAM_REQUEST = 1313;
+    static LocationManager locationManager;
+    static Location loc;
+    static LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_damage_reports);
 
+        //Initialize variables on create of activity.
         btnTakePhoto = (Button) findViewById(R.id.camerabutton);
         imgTakenPhoto = (ImageView) findViewById(R.id.imageview1);
         text9 = (EditText) findViewById(R.id.dateEdit);
+        btnSubmit = (Button) findViewById(R.id.submitbutton);
 
         btnTakePhoto.setOnClickListener(new btnTakenPhotoClicker());
+        btnSubmit.setOnClickListener(new btnSubmit());
 
         //This line is used to hide keyboard on startup.
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+        //locationManager initialize
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        //Location settings with default methods.
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                loc = location;
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        //Request location updates from the network (wifi or cell tower).
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+
+    }
+
+    //Method that would use the location every time it is changed.
+    public void makeUseOfNewLocation(Location location){
 
     }
 
@@ -75,9 +129,21 @@ public class DamageReports extends ActionBarActivity {
         }
     }
 
+    /*Class for button click of the "submit" button.*/
+    class btnSubmit implements Button.OnClickListener{
 
+        //Show the lat and long dialog fragment.
+        @Override
+        public void onClick(View v){
+            DialogFragment newFragment = new ShowLocationFragment();
+            newFragment.show(getFragmentManager(), "location");
+        }
+    }
+
+    /*Class for button click of the "take photo" button.*/
     class btnTakenPhotoClicker implements Button.OnClickListener{
 
+        //What to do on click.
         @Override
         public void onClick(View v) {
             Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -91,9 +157,10 @@ public class DamageReports extends ActionBarActivity {
         newFragment.show(getFragmentManager(), "datepicker");
     }
 
-    //Class to create a date picker fragment.
+    /*Class to create a date picker fragment.*/
     public static class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener{
 
+        //Method that sets default date on creation of the date picker.
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             // Use the current date as the default date in the picker
@@ -106,9 +173,43 @@ public class DamageReports extends ActionBarActivity {
             return new DatePickerDialog(getActivity(), this, year, month, day);
         }
 
+        //Method to set the text field for date to the user picked date.
         @Override
         public void onDateSet(DatePicker view, int year, int month, int day) {
             DamageReports.text9.setText(month+1 + "/" + day + "/" + year);
         }
     }
+
+    /*Class to show the dialog on press of the submit button.*/
+    public static class ShowLocationFragment extends DialogFragment{
+
+        //Method that will make everything upon creation of the fragment.
+        @Override
+        public Dialog onCreateDialog (Bundle savedInstanceState){
+            // Use the Builder class for convenient dialog construction
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+            //Variables that will get lat and long from the Location object.
+            double lat = loc.getLatitude();
+            double longit = loc.getLongitude();
+
+            //Setting the builder to have a message and options for "okay" and "cancel".
+            builder.setMessage("Latitude: " + Double.toString(lat) + " \nLongitude: " + Double.toString(longit))
+                    .setPositiveButton(R.string.okay, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // FIRE ZE MISSILES!
+                            locationManager.removeUpdates(locationListener);
+                        }
+                    })
+                    .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // User cancelled the dialog
+                            locationManager.removeUpdates(locationListener);
+                        }
+                    });
+            // Create the AlertDialog object and return it
+            return builder.create();
+        }
+    }
 }
+
