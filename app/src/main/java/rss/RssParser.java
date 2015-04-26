45b34c6e163b48e143d43d387b465429126cc197
laloC <laloC@users.noreply.github.com>
2015-04-26 15:06:23 -0500

    Update RssParser.java

--- app/src/main/java/rss/RssParser.java
+++ app/src/main/java/rss/RssParser.java
@@ -1,3 +1,23 @@
+/*
+*
+*  Copyright 2015 University of Wisconsin - Parkside
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*
+*/
+
+
 package rss;
 
 import android.util.Xml;
@@ -10,24 +30,10 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Copyright [2015] [University of Wisconsin - Parkside]
- * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Parses the XML RSS that is returned and creates separate RSS objects
- * with the data.
- */
+
+ //Parses the XML RSS that is returned and creates separate RSS objects
+ // with the data.
+ 
 public class RssParser {
 
     // We don't use namespaces
