b05503937a34cf87c57c6a263d23191e971be125
laloC <laloC@users.noreply.github.com>
2015-04-26 15:10:53 -0500

    Update MainActivity.java

--- app/src/main/java/u/ready_wisc/MainActivity.java
+++ app/src/main/java/u/ready_wisc/MainActivity.java
@@ -1,3 +1,23 @@
+/*
+*
+*  Copyright 2015 University of Wisconsin - Parkside
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*
+*/
+
+
 package u.ready_wisc;
 
 import android.content.ContentValues;
@@ -77,10 +97,10 @@ public class MainActivity extends ActionBarActivity {
                 //this string can be dynamically allocated later, I hardcoded just for the proof of concept
                 //String[][] webNames = new String[3][3];
 
-                /*new runnable object used to pull the data from the web.
-                android will only let you do json calls from a thread other
-                than the main
-                 */
+                //new runnable object used to pull the data from the web.
+                //android will only let you do json calls from a thread other
+                //than the main
+                
 
                 DBUpdateFromWeb foo = new DBUpdateFromWeb();
                 Thread t = new Thread(foo);
