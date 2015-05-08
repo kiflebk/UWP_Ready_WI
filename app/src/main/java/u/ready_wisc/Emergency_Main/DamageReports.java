1a12b4f107c863580f0e608d9572bc3146a516f5
piela001 <piela001@hotmail.com>
2015-05-08 12:13:52 -0500

    Fixed image send bug

--- app/src/main/java/u/ready_wisc/Emergency_Main/DamageReports.java
+++ app/src/main/java/u/ready_wisc/Emergency_Main/DamageReports.java
@@ -338,7 +338,7 @@ public class DamageReports extends ActionBarActivity {
                 obj.put("damage_desc", damage_desc.getText().toString().replace(" ", "%20"));
 
                 //TODO apache server needs to have url length parameter changed
-                obj.put("encoded_image", encodedString.replace("/", "%2F").replace("+", "%2B"));
+                obj.put("encoded_image", encodedString.replace("/", "%2F").replace("+", "%2B").replace("\n",""));
 
                 if (isOnline()) {
                     obj.put("longitude", (loc.getLongitude()+"").replace(" ","%20"));
@@ -463,7 +463,7 @@ public class DamageReports extends ActionBarActivity {
                 options.inSampleSize = 3;
                 ByteArrayOutputStream stream = new ByteArrayOutputStream();
                 // Must compress the Image to reduce image size to make upload easy
-                thumbnail.compress(Bitmap.CompressFormat.JPEG, 10, stream);  /// I changed this from PNG and 50
+                thumbnail.compress(Bitmap.CompressFormat.JPEG, 8, stream);  /// I changed this from PNG and 50
                 byte[] byte_arr = stream.toByteArray();
                 // Encode Image to String
                 encodedString = Base64.encodeToString(byte_arr, 0);
